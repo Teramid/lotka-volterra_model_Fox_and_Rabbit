1099b1cf4acde3cacbcfdbedaf00dde19a420fa7
Teramid <pszczotka1@gmail.com>
2023-07-31 14:55:40 +0200

    population charts, fix matrix, buttons func

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from matplotlib import markers
 from matplotlib.colors import ListedColormap
 import numpy as np
 import PyQt6.QtWidgets as qtw
@@ -7,6 +8,8 @@ import matplotlib
 
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
+
+matplotlib.rcParams.update({'font.size': 6})
 
 
 class UI(qtw.QMainWindow):
@@ -78,7 +81,10 @@ class UI(qtw.QMainWindow):
         self.map_colors = ListedColormap(['white', 'red', 'green'])
         
         self.view_widget = self.findChild(qtw.QWidget, "view_widget")
+        self.chart1_widget = self.findChild(qtw.QWidget, "chart1_widget")
+        self.chart2_widget = self.findChild(qtw.QWidget, "chart2_widget")
         
+        # View widget
         self.fig = Figure()
         self.ax = self.fig.add_subplot()
         self.plot = self.ax.pcolormesh(self.matrixSim, cmap=self.map_colors, edgecolors='k', linewidth=0)
@@ -87,12 +93,45 @@ class UI(qtw.QMainWindow):
         self.canvas = FigureCanvas(self.fig)
         self.layout = qtw.QVBoxLayout(self.view_widget)
         self.layout.addWidget(self.canvas)
-        self.fig.tight_layout(pad=0)
+        self.fig.tight_layout(pad=0.1)
+        
+        self.population_dependency = np.array([], dtype=[('population_fox', '<f4'), ('population_rabbit', '<f4')])
+        # Chart 1 widget
+        self.fig1 = Figure()
+        self.ax1 = self.fig1.add_subplot()
+        self.ax1.set(xlim=(0, 10), ylim=(0,30))
+        self.plot1 = self.ax1.plot(0, 0, linewidth= 1)
+        
+        self.canvas1 = FigureCanvas(self.fig1)
+        self.layout1 = qtw.QVBoxLayout(self.chart1_widget)
+        self.layout1.addWidget(self.canvas1)
+        self.fig1.tight_layout(pad=4)
+        
+        self.chart_2_datas_rabbit = np.array([], dtype=[('population', '<i4'),('time', '<i4')])
+        self.chart_2_datas_fox = np.array([], dtype=[('population', '<i4'),('time', '<i4')])
+        # Chart 2 widget
+        self.fig2 = Figure()
+        self.ax2 = self.fig2.add_subplot()
+        self.ax2.set(xlim=(0, 100), ylim=(0,40))
+        self.plot2 = self.ax2.plot(0, 0, linewidth = 1)
+        
+        self.canvas2 = FigureCanvas(self.fig2)
+        self.layout2 = qtw.QVBoxLayout(self.chart2_widget)
+        self.layout2.addWidget(self.canvas2)
+        self.fig2.tight_layout(pad=4)
         
         
+        # Define the button
         self.start_pushButton = self.findChild(qtw.QPushButton, "start_pushButton")
-        self.start_pushButton.clicked.connect(self.start_button_down)
+        self.step_pushButton = self.findChild(qtw.QPushButton, "step_pushButton")
+        self.reset_pushButton = self.findChild(qtw.QPushButton, "reset_pushButton")
         
+        self.reset_pushButton.setEnabled(False)
+        self.reset_pushButton.hide()
+        
+        self.start_pushButton.clicked.connect(self.start_button_down)
+        self.step_pushButton.clicked.connect(self.step_button_down)
+        self.reset_pushButton.clicked.connect(self.reset_button_down)
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.update_model)
         
@@ -133,7 +172,7 @@ class UI(qtw.QMainWindow):
         self.layout.removeWidget(self.canvas)
         self.canvas = FigureCanvas(self.fig)
         self.layout.addWidget(self.canvas)
-        self.fig.tight_layout(pad=0)
+        self.fig.tight_layout(pad=0.1)
         
     def slide_initialRabbit(self,value):
         self.initialRabbits_label.setText(str(value))
@@ -173,7 +212,51 @@ class UI(qtw.QMainWindow):
         value /=10
         self.simulationSpeed_label.setText(str(value))
         self.simulationSpeed=value
+        self.timer.setInterval(int(1001 - (1000*self.simulationSpeed)))
+    
+    
+    def update_chart_2_and_1(self,rabbit_number_temp,fox_number_temp):
+        population_dependency_temp = np.array([(rabbit_number_temp,fox_number_temp)], dtype=[('population_fox', '<f4'), ('population_rabbit', '<f4')])
         
+        rabbit_number_temp = np.array([(rabbit_number_temp, len(self.chart_2_datas_rabbit))], dtype=[('population', '<f4'), ('time', '<i4')])
+        fox_number_temp = np.array([(fox_number_temp, len(self.chart_2_datas_rabbit))], dtype=[('population', '<f4'), ('time', '<i4')])
+        
+        if len(self.chart_2_datas_rabbit)<100:
+            self.chart_2_datas_rabbit = np.append(self.chart_2_datas_rabbit, rabbit_number_temp, axis=0)
+            self.chart_2_datas_fox = np.append(self.chart_2_datas_fox, fox_number_temp, axis=0)
+            
+            self.population_dependency = np.append(self.population_dependency, population_dependency_temp, axis=0)
+            
+        else:
+            self.chart_2_datas_rabbit = np.append(self.chart_2_datas_rabbit[1:], rabbit_number_temp, axis=0)
+            self.chart_2_datas_fox = np.append(self.chart_2_datas_fox[1:], fox_number_temp, axis=0)
+            self.chart_2_datas_rabbit['time'] = np.arange(len(self.chart_2_datas_rabbit))
+            self.chart_2_datas_fox['time'] = np.arange(len(self.chart_2_datas_fox))
+            
+            self.population_dependency = np.append(self.population_dependency[1:], population_dependency_temp, axis=0)
+            
+            
+        
+        self.ax2.clear()
+        ylim = (int(max(max(self.chart_2_datas_rabbit['population']), max(self.chart_2_datas_fox['population'])) / 10) + 1) * 10
+        self.ax2.set(xlim=(0, 100), ylim=(0,ylim))
+        self.ax2.plot('time', 'population',data=self.chart_2_datas_rabbit, color= 'green', linewidth = 1)
+        self.ax2.plot('time', 'population',data=self.chart_2_datas_fox,color= 'red', linewidth = 1)
+        
+        self.canvas2.draw_idle()
+        
+        
+        self.ax1.clear()
+        ylim = (int(max(self.population_dependency['population_rabbit'])/10)+1) * 10
+        xlim = (int(max(self.population_dependency['population_fox'])/10)+1) * 10
+        self.ax1.set(xlim=(0, xlim), ylim=(0,ylim))
+        self.ax1.plot('population_fox', 'population_rabbit','bo', data=self.population_dependency, markersize= 3)
+        
+        self.canvas1.draw_idle()
+            
+
+            
+    
     def update_model(self):
         def random_move(x,y):
             direction = np.random.choice(['up_left', 'up', 'up_right', 
@@ -207,25 +290,111 @@ class UI(qtw.QMainWindow):
                 x = min(x + 1, self.worldSize - 1)
                 y = min(y + 1, self.worldSize - 1)
             return x,y
-
+        rabbit_number_temp = np.count_nonzero(self.matrixSim == 2)/(self.worldSize)
+        fox_number_temp = np.count_nonzero(self.matrixSim == 1)/(self.worldSize)
+        print(f"Rabbit: {rabbit_number_temp}%, fox: {fox_number_temp}%")
+        self.update_chart_2_and_1(rabbit_number_temp,fox_number_temp)
+        
         for value in [1,2]:
             x, y = np.where(self.matrixSim == value)
             self.matrixSim[x,y] = 0
             
-            for i in range(len(x)):
+            for i in range(max(len(x),len(y))):
                 new_x, new_y = x[i],y[i]
                 new_x, new_y = random_move(new_x, new_y)
-                while self.matrixSim[new_x, new_y] != 0:
+                status = 0
+                if value == 2 and np.random.rand(1,1) <= self.breedingRabbits:
+                    newBorn_x, newBorn_y = random_move(new_x, new_y)
+                    stat = 0
+                    while self.matrixSim[newBorn_x, newBorn_y] != 0 and stat < 15:
+                        newBorn_x, newBorn_y = random_move(new_x, new_y)
+                        stat +=1
+                    self.matrixSim[newBorn_x, newBorn_y] = value
+                while status<20:
                     new_x, new_y = random_move(new_x, new_y)
-                x[i], y[i] = new_x, new_y
+                    if self.matrixSim[new_x, new_y] == 0:
+                        status = 100
+                        if value == 2 and np.random.rand(1,1) <= self.breedingRabbits:
+                            newBorn_x, newBorn_y = x[i], y[i]
+                            x = np.append(x, newBorn_x)
+                            y = np.append(y, newBorn_y)
+                        
+                    elif value == 1 and self.matrixSim[new_x, new_y] == 1:
+                        status+=1
+                    elif value == 2 and self.matrixSim[new_x, new_y] == 2:
+                        status+=1
+                    elif value == 1 and self.matrixSim[new_x, new_y] == 2:
+                        if np.random.rand(1,1) < self.effectivenessFoxes:
+                            status = 100
+                            if np.random.rand(1,1) < self.breedingFoxes:
+                                newBorn_x, newBorn_y = random_move(new_x, new_y)
+                                while self.matrixSim[newBorn_x, newBorn_y] != 0 and newBorn_x != new_x and newBorn_y != new_y:
+                                    newBorn_x, newBorn_y = random_move(new_x, new_y)
+                                self.matrixSim[newBorn_x, newBorn_y] = value
+                                
+                        else:
+                            status+=1
+                    else:
+                        status+=1
+                if (value == 1 and np.random.rand(1,1) > self.mortalityFoxes) or value==2:   
+                    self.matrixSim[new_x, new_y] = value
+                else: pass
+                    
             
-            self.matrixSim[x, y] = value
         self.plot.set_array(self.matrixSim.ravel())
         self.canvas.draw_idle()
+        
     
     def start_button_down(self):
-        #self.update_model()
-        self.timer.start(500)
+        if self.start_pushButton.text() == "Start":
+            simSpeed = int(1001 - (1000*self.simulationSpeed))
+            self.timer.start(simSpeed)
+            #self.start_pushButton.setEnabled(False)
+            self.worldSize_horizontalSlider.setEnabled(False)
+            self.initialRabbit_horizontalSlider.setEnabled(False)
+            self.initialFoxes_horizontalSlider.setEnabled(False)
+            self.breedingRabbits_horizontalSlider.setEnabled(False)
+            self.breedingFoxes_horizontalSlider.setEnabled(False)
+            self.mortalityFoxes_horizontalSlider.setEnabled(False)
+            self.effectivenessFoxes_horizontalSlider.setEnabled(False)
+            self.start_pushButton.setText('Stop')
+            self.reset_pushButton.setEnabled(False)
+            self.reset_pushButton.hide()
+            self.step_pushButton.setEnabled(False)
+            self.step_pushButton.hide()
+        else:
+            self.timer.stop()
+            #self.start_pushButton.setEnabled(True)
+            self.worldSize_horizontalSlider.setEnabled(True)
+            self.initialRabbit_horizontalSlider.setEnabled(True)
+            self.initialFoxes_horizontalSlider.setEnabled(True)
+            self.breedingRabbits_horizontalSlider.setEnabled(True)
+            self.breedingFoxes_horizontalSlider.setEnabled(True)
+            self.mortalityFoxes_horizontalSlider.setEnabled(True)
+            self.effectivenessFoxes_horizontalSlider.setEnabled(True)
+            self.start_pushButton.setText('Start')
+            self.reset_pushButton.setEnabled(True)
+            self.reset_pushButton.show()
+            self.step_pushButton.setEnabled(True)
+            self.step_pushButton.show()
+
+    def step_button_down(self):
+        self.update_model()
+        self.reset_pushButton.setEnabled(True)
+        self.reset_pushButton.show()
+        
+        
+    def reset_button_down(self):
+        self.reset_pushButton.setEnabled(False)
+        self.reset_pushButton.hide()
+        self.chart_2_datas_rabbit = np.array([], dtype=[('population', '<i4'),('time', '<i4')])
+        self.chart_2_datas_fox = np.array([], dtype=[('population', '<i4'),('time', '<i4')])
+        self.population_dependency = np.array([], dtype=[('population_fox', '<f4'), ('population_rabbit', '<f4')])
+        self.redraw_view()
+        self.ax1.clear()
+        self.ax2.clear()
+        self.canvas1.draw_idle()
+        self.canvas2.draw_idle()
 
 
 
